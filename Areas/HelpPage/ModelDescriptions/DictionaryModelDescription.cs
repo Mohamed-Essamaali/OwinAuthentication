526c3eaaa82b896591902f0d509f3a457d7db40e
user2 <user2@DESKTOP-44R6R6H>
2021-11-09 21:13:51 -0500

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OwinAuthentication.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/POCController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OwinAuthentication.Controllers
+{
+    [Authorize]
+    public class POCController : ApiController
+    {
+        [HttpGet]
+        [Route("api/TestMethod")]
+        public string TestMethod()
+        {
+            return "Hello, C# Corner Member. ";
+        }
+    }
+}
